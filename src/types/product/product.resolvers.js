889ab02fcdf47c679b804eef9f42f93215dd8e28
kirilchristov <kiro.mm@gmail.com>
2019-11-30 18:32:13 -0800

    lesson 2

--- src/types/product/product.resolvers.js
+++ src/types/product/product.resolvers.js
@@ -9,10 +9,48 @@ const productsTypeMatcher = {
   DRONE: 'Drone'
 }
 
+const newProduct(_, args, ctx) => {
+  return Product.create({...args.input, createdBy: ctx.user._id})// We got this from Mongo mondel
+}
+
+const updateProduct(_, args) => {
+  return Product.findByIdAndUpdate(args.id,args.input, {new: true}) //new: true returns theproject after update
+  .lean()
+  .exec()
+}
+
+const removeProduct(_,args) => {
+  return Product.findByIdAndRemove(args.id)
+  .lean()
+  .exec()
+}
+
+const product = (_,args) => {
+  return Product.findById(args.id)
+  .lean()
+  .exec()
+}
+
+const products = () => {
+  return Product.find({}).exec()
+}
+
 export default {
-  Query: {},
-  Mutation: {},
+  Query: {
+    product,
+    products
+  },
+  Mutation: {
+    newProduct,
+    updateProduct,
+    removeProduct
+  },
   Product: {
-    __resolveType(product) {}
+    __resolveType(product) {},
+    createdBy(product){
+      return Product.findById(product.createdBy)
+      .lean()
+      .exec()
+    }
   }
 }
